d3ce70ee3ee6dffbe63989a5fe7f6f0fff93f8a4
mybss <37547093+mybss@users.noreply.github.com>
2023-03-26 23:20:54 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
-set GIT=
+set PYTHON="C:\Program Files\Python310\python.exe"
+set GIT="C:\Users\bk\AppData\Local\GitHubDesktop\GitHubDesktop.exe"
 set VENV_DIR=
 set COMMANDLINE_ARGS=
-
+git pull
 call webui.bat
